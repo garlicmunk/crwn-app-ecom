0bfebe576b5197ca568fe341f6e36d7d8b0e50f4
garlicmunk <venubhargavalluri@gmail.com>
2020-11-22 02:45:27 +0530

    initialising project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import HomePage from './homepage.component';
+
+function App() {
+  
+  return (
+    <div>
+      <HomePage title="HATS" />
+      <HomePage title="JACKETS"/>
+      <HomePage title="SNEAKERS"/>
+      <HomePage title="WOMENS"/>
+      <HomePage title="MENS"/>
+    </div>
+  );
+}
+
+export default App;
